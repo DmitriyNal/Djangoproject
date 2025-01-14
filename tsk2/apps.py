003c5518e4c64feb5ea1da4b2ed3f6e7548af24d
kiper <dima7kiper@mail.ru>
2025-01-14 14:16:18 +0300

    Изменил шаблоны html , перепесал представления в файле views.py для приложения tsk2 c использованием словаря

---
+++ tsk2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Tsk2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tsk2'
--- tsk2/views.py
+++ tsk2/views.py
@@ -3,12 +3,23 @@ from django.views.generic import TemplateView
 
 
 def index(request):
-    return render(request, "main.html")
+    title = 'Главная страница'
+    auto = 'Автомобили в наличии'
+    offers = 'Специальные предложения'
+    context = {'title': title, 'auto': auto, 'offers': offers}
+
+    return render(request, "second_tsk/main.html", context)
 
 
 def home_page(request):
-    return render(request, "Base.html")
+    title = 'Главная страница'
+    shop = 'Магазин'
+    credit = 'Кредит'
+    choise = 'Выберите раздел'
+    context = {'title': title, 'shop': shop, 'credit': credit, 'choise': choise}
+
+    return render(request, "second_tsk/Base.html", context)
 
 
 class IndexView(TemplateView):
-    template_name = 'kredit.html'
+    template_name = 'second_tsk/kredit.html'
